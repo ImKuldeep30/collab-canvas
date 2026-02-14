62c958ac0294925b3b5d648b00b4bd3b1db0a5ff
kuldeep <kuldpkohli2003@gmail.com>
2026-02-14 21:51:15 +0530

    Initial commit: Setup backend structure and Docker configuration

---
+++ controllers/authController.js
@@ -0,0 +1,50 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+exports.signup = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const user = new User({ name, email, password });
+    await user.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+
+  } catch (error) {
+    console.error("Signup Error:", error);
+    res.status(500).json({ error: error.message });
+  }
+
+};
+
+exports.signin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.json({ token });
+
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+};
---
+++ server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authroutes");
+
+const app = express();
+const PORT = 3000;
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth",authRoutes);
+
+app.listen(PORT, "0.0.0.0",()=>{
+    console.log(`server is running on http://localhost:${PORT}`);
+});
+
